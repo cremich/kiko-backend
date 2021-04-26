fbf72199f05405d648fae7bcdba3872396c2199e
christian.rest <christian.rest@7p-group.com>
2021-04-26 21:28:16 +0200

    task: trim and hash mail addresses for alarm topic subscriptions for ID reproduce-ability

--- lib/shared/constructs/alarm-infra.ts
+++ lib/shared/constructs/alarm-infra.ts
@@ -1,6 +1,7 @@
 import * as cdk from "@aws-cdk/core";
 import { Stack } from "@aws-cdk/core";
 import { ITopic, Subscription, SubscriptionProtocol, Topic } from "@aws-cdk/aws-sns";
+import { createHash } from "crypto";
 
 interface TopicEmailSubscriptionsProps {
   readonly deployStage: string;
@@ -16,13 +17,18 @@ export class AlarmInfra extends Stack {
     this.topic = new Topic(this, `${props.deployStage}-kiko-alarm-topic`);
 
     if (props.commaSeperatedEmailAddresses) {
-      props.commaSeperatedEmailAddresses.split(",").forEach((mailAddress, idx) => {
-        new Subscription(this, `${props.deployStage}-kiko-alarm-topic-subscription-${idx}`, {
-          topic: this.topic,
-          protocol: SubscriptionProtocol.EMAIL,
-          endpoint: mailAddress,
+      props.commaSeperatedEmailAddresses
+        .split(",")
+        .map((mailAddress) => mailAddress.trim())
+        .forEach((mailAddress) => {
+          const mailHash = createHash("md5").update(mailAddress).digest("hex");
+
+          new Subscription(this, `${props.deployStage}-kiko-alarm-topic-subscription-${mailHash}`, {
+            topic: this.topic,
+            protocol: SubscriptionProtocol.EMAIL,
+            endpoint: mailAddress,
+          });
         });
-      });
     }
   }
 }
