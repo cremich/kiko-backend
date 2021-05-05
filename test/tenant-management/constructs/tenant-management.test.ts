fc314979babc99b1669ddcf372805a178a458a04
Christian Bonzelet <christian.bonzelet@gmail.com>
2021-05-05 21:04:14 +0200

    feat: automate tenant onboarding using lambda function

--- test/tenant-management/constructs/tenant-management.test.ts
+++ test/tenant-management/constructs/tenant-management.test.ts
@@ -1,30 +1,33 @@
 import * as cdk from "@aws-cdk/core";
 import "@aws-cdk/assert/jest";
+import * as cloudfront from "@aws-cdk/aws-cloudfront";
+import * as origins from "@aws-cdk/aws-cloudfront-origins";
 import * as dynamodb from "@aws-cdk/aws-dynamodb";
 import * as sns from "@aws-cdk/aws-sns";
-import { TenantManagement } from "../../../lib/tenant-management/constructs/tenant-management";
+import { TenantManagement, TenantManagementProps } from "../../../lib/tenant-management/constructs/tenant-management";
 import * as cognito from "@aws-cdk/aws-cognito";
 
 let stack: cdk.Stack;
-let alarmTopic: sns.Topic;
-let poolTable: dynamodb.Table;
+let props: TenantManagementProps;
 
 beforeEach(() => {
   stack = new cdk.Stack();
-  alarmTopic = new sns.Topic(stack, "alarm-topic", {});
-  poolTable = new dynamodb.Table(stack, "pool-table", {
-    partitionKey: { name: "tenant", type: dynamodb.AttributeType.STRING },
-    sortKey: { name: "poolName", type: dynamodb.AttributeType.STRING },
-  });
+  props = {
+    alarmTopic: new sns.Topic(stack, "alarm-topic", {}),
+    poolTable: new dynamodb.Table(stack, "pool-table", {
+      partitionKey: { name: "tenant", type: dynamodb.AttributeType.STRING },
+      sortKey: { name: "poolName", type: dynamodb.AttributeType.STRING },
+    }),
+    deployStage: "test",
+    tenants: [],
+    cloudfrontDistribution: new cloudfront.Distribution(stack, "cloudfront-distribution", {
+      defaultBehavior: { origin: new origins.HttpOrigin("www.example.com") },
+    }),
+  };
 });
 
 test("Cognito user pool created with self signup deactivated", () => {
-  new TenantManagement(stack, "tenant-management", {
-    poolTable,
-    deployStage: "test",
-    alarmTopic,
-    tenants: [],
-  });
+  new TenantManagement(stack, "tenant-management", props);
 
   expect(stack).toHaveResourceLike("AWS::Cognito::UserPool", {
     AdminCreateUserConfig: {
@@ -34,12 +37,7 @@ test("Cognito user pool created with self signup deactivated", () => {
 });
 
 test("Cognito user pool schema contains only email address", () => {
-  new TenantManagement(stack, "tenant-management", {
-    poolTable,
-    deployStage: "test",
-    alarmTopic,
-    tenants: [],
-  });
+  new TenantManagement(stack, "tenant-management", props);
 
   expect(stack).toHaveResourceLike("AWS::Cognito::UserPool", {
     Schema: [
@@ -53,24 +51,14 @@ test("Cognito user pool schema contains only email address", () => {
 });
 
 test("Cognito user pool resource is not replaced due to logical id change", () => {
-  const tenantManagement = new TenantManagement(stack, "tenant-management", {
-    poolTable,
-    deployStage: "test",
-    alarmTopic,
-    tenants: [],
-  });
+  const tenantManagement = new TenantManagement(stack, "tenant-management", props);
 
   const userPoolLogicalId = stack.getLogicalId(tenantManagement.userPool.node.defaultChild as cognito.CfnUserPool);
   expect(userPoolLogicalId).toEqual("tenantmanagementkikouserpool9AC1D4C7");
 });
 
 test("Cognito user pool sign in alias set to email", () => {
-  new TenantManagement(stack, "tenant-management", {
-    poolTable,
-    deployStage: "test",
-    alarmTopic,
-    tenants: [],
-  });
+  new TenantManagement(stack, "tenant-management", props);
 
   expect(stack).toHaveResourceLike("AWS::Cognito::UserPool", {
     UsernameAttributes: ["email"],
@@ -78,12 +66,7 @@ test("Cognito user pool sign in alias set to email", () => {
 });
 
 test("Cognito user pool has web client configured", () => {
-  const tenantManagement = new TenantManagement(stack, "tenant-management", {
-    poolTable,
-    deployStage: "test",
-    alarmTopic,
-    tenants: [],
-  });
+  const tenantManagement = new TenantManagement(stack, "tenant-management", props);
   const userPoolLogicalId = stack.getLogicalId(tenantManagement.userPool.node.defaultChild as cognito.CfnUserPool);
 
   expect(stack).toHaveResourceLike("AWS::Cognito::UserPoolClient", {
@@ -95,12 +78,7 @@ test("Cognito user pool has web client configured", () => {
 });
 
 test("Pinpoint application created", () => {
-  new TenantManagement(stack, "tenant-management", {
-    poolTable,
-    deployStage: "test",
-    alarmTopic,
-    tenants: [],
-  });
+  new TenantManagement(stack, "tenant-management", props);
 
   expect(stack).toHaveResource("AWS::Pinpoint::App", {
     Name: "kiko",
@@ -108,24 +86,14 @@ test("Pinpoint application created", () => {
 });
 
 test("Pinpoint application resource is not replaced due to logical id change", () => {
-  const tenantManagement = new TenantManagement(stack, "tenant-management", {
-    poolTable,
-    deployStage: "test",
-    alarmTopic,
-    tenants: [],
-  });
+  const tenantManagement = new TenantManagement(stack, "tenant-management", props);
 
   const pinpointLogicalId = stack.getLogicalId(tenantManagement.pinpointApplication);
   expect(pinpointLogicalId).toEqual("tenantmanagementpinpointapplicationAF757B26");
 });
 
 test("SMS channel created", () => {
-  const tenantManagement = new TenantManagement(stack, "tenant-management", {
-    poolTable,
-    deployStage: "test",
-    alarmTopic,
-    tenants: [],
-  });
+  const tenantManagement = new TenantManagement(stack, "tenant-management", props);
 
   const pinpointLogicalId = stack.getLogicalId(tenantManagement.pinpointApplication);
 
@@ -139,15 +107,10 @@ test("SMS channel created", () => {
 });
 
 test("CampaignSendMessagePermanentFailure alarm is activated", () => {
-  const tenantManagement = new TenantManagement(stack, "tenant-management", {
-    poolTable,
-    deployStage: "test",
-    alarmTopic,
-    tenants: [],
-  });
+  const tenantManagement = new TenantManagement(stack, "tenant-management", props);
 
   const pinpointLogicalId = stack.getLogicalId(tenantManagement.pinpointApplication);
-  const topicLogicalId = stack.getLogicalId(alarmTopic.node.defaultChild as sns.CfnTopic);
+  const topicLogicalId = stack.getLogicalId(props.alarmTopic.node.defaultChild as sns.CfnTopic);
 
   expect(stack).toHaveResource("AWS::CloudWatch::Alarm", {
     ComparisonOperator: "GreaterThanOrEqualToThreshold",
